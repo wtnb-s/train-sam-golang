a82d935332016b420198d450a0579b6bc2a863c7
wtnb-s <denebu.1.4@gmail.com>
2021-05-10 23:18:10 +0900

    Dynamobdをローカルと本番で切り替えられるようにソースを修正

--- score-register/main.go
+++ score-register/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,13 +13,15 @@ import (
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// 環境変数設定
+	endpoint := os.Getenv("DYNAMODB_ENDPOINT")
 
 	// リクエストボディ取得
 	reqBody := request.Body
 	jsonBytes := ([]byte)(reqBody)
 	presonReq := new(PersonRequest)
 	if err := json.Unmarshal(jsonBytes, presonReq); err != nil {
-		fmt.Println("[ERROR]", err)
+		return events.APIGatewayProxyResponse{}, err
 	}
 	personID := presonReq.PersonID
 	personName := presonReq.PersonName
@@ -30,7 +32,6 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		passingMark = true
 	}
 
-	// DynamoDBへ永続化
 	person := Person{
 		PersonID:    personID,
 		PersonName:  personName,
@@ -38,21 +39,27 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		Score:       score,
 		PassingMark: passingMark,
 	}
+	// item を dynamodb attributeに変換
 	av, err := dynamodbattribute.MarshalMap(person)
 	if err != nil {
-		fmt.Println("[ERROR]", err)
+		return events.APIGatewayProxyResponse{}, err
 	}
 
-	session, err := session.NewSession()
-	conn := dynamodb.New(session)
-	param, err := conn.PutItem(&dynamodb.PutItemInput{
+	// Dynamodb接続設定
+	session := session.Must(session.NewSession())
+	config := aws.NewConfig().WithRegion("ap-northeast-1")
+	if len(endpoint) > 0 {
+		config = config.WithEndpoint(endpoint)
+	}
+	db := dynamodb.New(session, config)
+
+	_, err = db.PutItem(&dynamodb.PutItemInput{
 		TableName: aws.String("Score"),
 		Item:      av,
 	})
 	if err != nil {
-		fmt.Println("[ERROR]", err)
+		return events.APIGatewayProxyResponse{}, err
 	}
-	fmt.Println(param)
 
 	return events.APIGatewayProxyResponse{
 		Body:       string(jsonBytes),
